11a50515085ba90c23a3844fe1c4b3f05609efe7
PariveshitaThota <77807971+PariveshitaThota@users.noreply.github.com>
2022-04-18 12:51:52 -0500

    added changes
    added changes

--- Pariveshita/PyFlink-G05/src/main/java/org/apache/beam/examples/MinimalPageRankThota.java
+++ Pariveshita/PyFlink-G05/src/main/java/org/apache/beam/examples/MinimalPageRankThota.java
@@ -7,11 +7,13 @@ import org.apache.beam.sdk.io.TextIO;
 import org.apache.beam.sdk.options.PipelineOptions;
 import org.apache.beam.sdk.options.PipelineOptionsFactory;
 import org.apache.beam.sdk.transforms.Count;
+import org.apache.beam.sdk.transforms.GroupByKey;
 import org.apache.beam.sdk.transforms.Filter;
 import org.apache.beam.sdk.transforms.FlatMapElements;
 import org.apache.beam.sdk.transforms.Flatten;
 import org.apache.beam.sdk.transforms.MapElements;
 import org.apache.beam.sdk.values.KV;
+import org.apache.beam.sdk.transforms.GroupByKey;
 import org.apache.beam.sdk.values.TypeDescriptors;
 import org.apache.beam.sdk.values.PCollection;
 import org.apache.beam.sdk.values.PCollectionList;
@@ -20,41 +22,6 @@ import org.apache.beam.sdk.values.TypeDescriptor;
 
 
 public class MinimalPageRankThota {
-    // DEFINE DOFNS
-  // ==================================================================
-  // You can make your pipeline assembly code less verbose by defining
-  // your DoFns statically out-of-line.
-  // Each DoFn<InputT, OutputT> takes previous output
-  // as input of type InputT
-  // and transforms it to OutputT.
-  // We pass this DoFn to a ParDo in our pipeline.
-
-  /**
-   * DoFn Job1Finalizer takes KV(String, String List of outlinks) and transforms
-   * the value into our custom RankedPage Value holding the page's rank and list
-   * of voters.
-   * 
-   * The output of the Job1 Finalizer creates the initial input into our
-   * iterative Job 2.
-   */
-  static class Job1Finalizer extends DoFn<KV<String, Iterable<String>>, KV<String, RankedPage>> {
-    @ProcessElement
-    public void processElement(@Element KV<String, Iterable<String>> element,
-        OutputReceiver<KV<String, RankedPage>> receiver) {
-      Integer contributorVotes = 0;
-      if (element.getValue() instanceof Collection) {
-        contributorVotes = ((Collection<String>) element.getValue()).size();
-      }
-      ArrayList<VotingPage> voters = new ArrayList<VotingPage>();
-      for (String voterName : element.getValue()) {
-        if (!voterName.isEmpty()) {
-          voters.add(new VotingPage(voterName, contributorVotes));
-        }
-      }
-      receiver.output(KV.of(element.getKey(), new RankedPage(element.getKey(), voters)));
-    }
-  }
-
 
   public static void main(String[] args) {
     PipelineOptions options = PipelineOptionsFactory.create();
@@ -67,11 +34,15 @@ public class MinimalPageRankThota {
    PCollection<KV<String,String>> v2 = PariMapper01(p,"python.md",dataFolder);
    PCollection<KV<String,String>> v3 = PariMapper01(p,"java.md",dataFolder);
    PCollection<KV<String,String>> v4 = PariMapper01(p,"README.md",dataFolder);
+    PCollection<KV<String,String>> v5 = PariMapper01(p,"swift.md",dataFolder);
+
 
    
     PCollectionList<KV<String, String>> pCollectionList = PCollectionList.of(v1).and(v2).and(v3).and(v4);
     PCollection<KV<String, String>> mergedList = pCollectionList.apply(Flatten.<KV<String,String>>pCollections());
-    PCollection<String> pLinksString = mergedList.apply(MapElements.into(TypeDescriptors.strings()).via((mergeOut)->mergeOut.toString()));
+       PCollection<KV<String, Iterable<String>>> combinedList =mergedList.apply(GroupByKey.create());
+
+    PCollection<String> pLinksString = combinedList.apply(MapElements.into(TypeDescriptors.strings()).via((mergeOut)->mergeOut.toString()));
     pLinksString.apply(TextIO.write().to("parioutput"));  
     p.run().waitUntilFinish();
   }
--- Pariveshita/PyFlink-G05/src/nwm/python/flink/group/project/MinimalPageRankThota.java
+++ Pariveshita/PyFlink-G05/src/nwm/python/flink/group/project/MinimalPageRankThota.java
@@ -1,4 +1,4 @@
-package edu.nwmissouri.groupOfFive;
+package org.apache.beam.examples;
 import java.util.Arrays;
 
 // import org.apache.beam.model.pipeline.v1.RunnerApi.PCollection;
@@ -20,41 +20,6 @@ import org.apache.beam.sdk.values.TypeDescriptor;
 
 
 public class MinimalPageRankThota {
-    // DEFINE DOFNS
-  // ==================================================================
-  // You can make your pipeline assembly code less verbose by defining
-  // your DoFns statically out-of-line.
-  // Each DoFn<InputT, OutputT> takes previous output
-  // as input of type InputT
-  // and transforms it to OutputT.
-  // We pass this DoFn to a ParDo in our pipeline.
-
-  /**
-   * DoFn Job1Finalizer takes KV(String, String List of outlinks) and transforms
-   * the value into our custom RankedPage Value holding the page's rank and list
-   * of voters.
-   * 
-   * The output of the Job1 Finalizer creates the initial input into our
-   * iterative Job 2.
-   */
-  static class Job1Finalizer extends DoFn<KV<String, Iterable<String>>, KV<String, RankedPage>> {
-    @ProcessElement
-    public void processElement(@Element KV<String, Iterable<String>> element,
-        OutputReceiver<KV<String, RankedPage>> receiver) {
-      Integer contributorVotes = 0;
-      if (element.getValue() instanceof Collection) {
-        contributorVotes = ((Collection<String>) element.getValue()).size();
-      }
-      ArrayList<VotingPage> voters = new ArrayList<VotingPage>();
-      for (String voterName : element.getValue()) {
-        if (!voterName.isEmpty()) {
-          voters.add(new VotingPage(voterName, contributorVotes));
-        }
-      }
-      receiver.output(KV.of(element.getKey(), new RankedPage(element.getKey(), voters)));
-    }
-  }
-
 
   public static void main(String[] args) {
     PipelineOptions options = PipelineOptionsFactory.create();
