e66907dbf8dcdbc12880d860b1d49a24e1c4713f
JayaShankar Mangina <79549340+jyshnkr@users.noreply.github.com>
2022-04-19 21:52:01 -0500

    Added DoFn fucntion

--- JayaShankar/PyFlink-G05/src/main/java/edu/nwmissouri/groupOfFive/MinimalPageRankJayaShankar.java
+++ JayaShankar/PyFlink-G05/src/main/java/edu/nwmissouri/groupOfFive/MinimalPageRankJayaShankar.java
@@ -17,6 +17,8 @@
  */
 package edu.nwmissouri.groupOfFive;
 
+import java.util.ArrayList;
+
 // beam-playground:
 //   name: MinimalWordCount
 //   description: An example that counts words in Shakespeare's works.
@@ -31,11 +33,14 @@ package edu.nwmissouri.groupOfFive;
 //     - Quickstart
 
 import java.util.Arrays;
+import java.util.Collection;
+
 import org.apache.beam.sdk.Pipeline;
 import org.apache.beam.sdk.io.TextIO;
 import org.apache.beam.sdk.options.PipelineOptions;
 import org.apache.beam.sdk.options.PipelineOptionsFactory;
 import org.apache.beam.sdk.transforms.Count;
+import org.apache.beam.sdk.transforms.DoFn;
 import org.apache.beam.sdk.transforms.GroupByKey;
 import org.apache.beam.sdk.transforms.Filter;
 import org.apache.beam.sdk.transforms.FlatMapElements;
@@ -74,6 +79,40 @@ import org.apache.beam.sdk.values.TypeDescriptors;
  * file service.
  */
 public class MinimalPageRankJayaShankar {
+  // DEFINE DOFNS
+  // ==================================================================
+  // You can make your pipeline assembly code less verbose by defining
+  // your DoFns statically out-of-line.
+  // Each DoFn<InputT, OutputT> takes previous output
+  // as input of type InputT
+  // and transforms it to OutputT.
+  // We pass this DoFn to a ParDo in our pipeline.
+
+  /**
+   * DoFn Job1Finalizer takes KV(String, String List of outlinks) and transforms
+   * the value into our custom RankedPage Value holding the page's rank and list
+   * of voters.
+   * 
+   * The output of the Job1 Finalizer creates the initial input into our
+   * iterative Job 2.
+   */
+  static class Job1Finalizer extends DoFn<KV<String, Iterable<String>>, KV<String, RankedPage>> {
+    @ProcessElement
+    public void processElement(@Element KV<String, Iterable<String>> element,
+        OutputReceiver<KV<String, RankedPage>> receiver) {
+      Integer contributorVotes = 0;
+      if (element.getValue() instanceof Collection) {
+        contributorVotes = ((Collection<String>) element.getValue()).size();
+      }
+      ArrayList<VotingPage> voters = new ArrayList<VotingPage>();
+      for (String voterName : element.getValue()) {
+        if (!voterName.isEmpty()) {
+          voters.add(new VotingPage(voterName, contributorVotes));
+        }
+      }
+      receiver.output(KV.of(element.getKey(), new RankedPage(element.getKey(), voters)));
+    }
+  }
 
   public static void main(String[] args) {
     
---
+++ JayaShankar/PyFlink-G05/src/main/java/edu/nwmissouri/groupOfFive/RankedPage.java
@@ -0,0 +1,10 @@
+package edu.nwmissouri.groupOfFive;
+
+import java.util.ArrayList;
+
+public class RankedPage {
+
+    public RankedPage(String key, ArrayList<VotingPage> voters) {
+    }
+    
+}
