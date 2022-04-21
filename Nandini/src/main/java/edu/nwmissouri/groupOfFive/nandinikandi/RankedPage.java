b10213fc4a2ac6b03989c7c16d440986b898b729
nandinikandi <77817444+nandinikandi@users.noreply.github.com>
2022-04-21 17:42:07 -0500

    Update RankedPage.java

--- Nandini/src/main/java/edu/nwmissouri/groupOfFive/nandinikandi/RankedPage.java
+++ Nandini/src/main/java/edu/nwmissouri/groupOfFive/nandinikandi/RankedPage.java
@@ -1,31 +1,53 @@
 package edu.nwmissouri.groupOfFive.nandinikandi;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 
-public class RankedPage {
+public class RankedPage implements Serializable{
 
-    public String key;
-    public ArrayList<VotingPage> voters;
+    public String nameIn = "unknown.md";
+    public Double rank = 1.000;
+    public ArrayList<VotingPage> voters = new ArrayList<VotingPage>();
 
-    public RankedPage(String key, ArrayList<VotingPage> voters) {
-        this.key = key;
+
+    public RankedPage(String nameIn, ArrayList<VotingPage> voters) {
+        this.nameIn = nameIn;
         this.voters = voters;
     }
 
-    public String getKey(){
-        return key;
+    public RankedPage(String contributingPageName, Double contributingPageRank, ArrayList<VotingPage> voters) {
+        this.nameIn = contributingPageName;
+        this.rank = contributingPageRank;
+        this.voters = voters;
+    }
+
+    public String getNameIn(){
+        return nameIn;
     }
 
     public  ArrayList<VotingPage> getVoters(){
         return voters;
     }
 
-    public void setKey(String key){
-        this.key = key;
+    public Double getRank(){
+        return rank;
+    }
+
+    public void setRank(Double rank ){
+        this.rank = rank;
+    }
+
+    public void setNameIn(String nameIn){
+        this.nameIn = nameIn;
     }
 
     public  void setVoters(ArrayList<VotingPage> voters){
         this.voters = voters;
     }
+
+    @Override
+    public String toString(){
+        return String.format("%s, %.5s, %s", this.nameIn, this.rank, this.voters.toString());
+    } 
 
 }
