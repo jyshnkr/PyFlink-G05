c4e763065586893ca1f8ce7588f81a424d696c4e
Madhu Babu Arla <49851961+Madhuarla@users.noreply.github.com>
2022-04-29 14:49:39 -0500

    Update RankedPage.java

--- Madhu/PyFlink-G05/src/main/java/edu/nwmissouri/groupOfFive/RankedPage.java
+++ Madhu/PyFlink-G05/src/main/java/edu/nwmissouri/groupOfFive/RankedPage.java
@@ -1,10 +1,37 @@
 package edu.nwmissouri.groupOfFive;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 
-public class RankedPage {
+public class RankedPage implements Serializable{
 
-    public RankedPage(String key, ArrayList<VotingPage> voters) {
+    String name = "unknown.md";
+    Double rank = 1.000;
+
+    ArrayList<VotingPage> voters = new ArrayList<VotingPage>();
+
+    RankedPage(String nameIn, ArrayList<VotingPage> votersIn){
+        this.name = nameIn;
+        this.voters = votersIn;
+    }
+
+    RankedPage(String nameIn, Double rankIn, ArrayList<VotingPage> votersIn){
+        this.name = nameIn;
+        this.voters = votersIn;
+        this.rank = rankIn;
+    }
+
+    public Double getRank(){
+        return rank;
+    }
+    
+    public ArrayList<VotingPage> getVoters() {
+        return voters;
+    }
+    
+    @Override
+    public String toString() {
+        return String.format("%s,%.5f,%s", this.name,this.rank,this.voters.toString());
     }
     
 }
