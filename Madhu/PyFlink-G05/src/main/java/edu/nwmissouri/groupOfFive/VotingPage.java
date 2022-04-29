aecec5ddd07d5f0d8891e4eef9cebe3a085a1673
Madhu Babu Arla <49851961+Madhuarla@users.noreply.github.com>
2022-04-29 14:51:25 -0500

    Update VotingPage.java

--- Madhu/PyFlink-G05/src/main/java/edu/nwmissouri/groupOfFive/VotingPage.java
+++ Madhu/PyFlink-G05/src/main/java/edu/nwmissouri/groupOfFive/VotingPage.java
@@ -1,8 +1,39 @@
 package edu.nwmissouri.groupOfFive;
 
-public class VotingPage {
+import java.io.Serializable;
 
-    public VotingPage(String voterName, Integer contributorVotes) {
+public class VotingPage implements Serializable {
+
+    String name = "unknown.md";
+    Double rank = 1.0;
+    Integer votes = 0;
+
+    public VotingPage(String nameIn, Double rankIn, Integer votesIn){
+        this.name = nameIn;
+        this.rank = rankIn;
+        this.votes = votesIn;
+    }
+
+    public VotingPage(String nameIn, Integer votesIn) {
+        this.name = nameIn;
+        this.votes = votesIn;
+    }
+    
+    public String getName() {
+        return name;
+    }
+    
+    public Double getRank() {
+        return rank;
+    }
+    
+    public Integer getVotes() {
+        return votes;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s,%.5f,%s", this.name,this.rank,this.votes);
     }
     
 }
